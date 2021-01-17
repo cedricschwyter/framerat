ce420280864d2e829046ba48e0b7849e77bc8d9c
D3PSI <d3psigames@gmail.com>
2021-01-17 15:16:53 +0100

    Add RenderingContext incomplete base class

---
+++ framerat/core/rendering/opengl/OpenGLRenderingContext.hpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  OpenGLRenderingContext.hpp
+ *
+ *    Description:  Defines core OpenGL rendering context
+ *
+ *        Version:  1.0
+ *        Created:  01/17/21 14:56:56
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Cedric Schwyter (D3PSI)
+ *
+ * =====================================================================================
+ */
+#ifndef __FRAMERAT_CORE_RENDERING_OPENGL_OPENGLRENDERINGCONTEXT_HPP
+#define __FRAMERAT_CORE_RENDERING_OPENGL_OPENGLRENDERINGCONTEXT_HPP
+
+#include "../RenderingContext.hpp"
+
+namespace framerat::core::rendering::opengl {}
+
+#endif // __FRAMERAT_CORE_RENDERING_OPENGL_OPENGLRENDERINGCONTEXT_HPP
---
+++ framerat/core/rendering/vulkan/Vulkan.hpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Vulkan.hpp
+ *
+ *    Description:  Defines core Vulkan functionality
+ *
+ *        Version:  1.0
+ *        Created:  01/17/21 15:02:31
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Cedric Schwyter (D3PSI)
+ *
+ * =====================================================================================
+ */
+#ifndef __FRAMERAT_CORE_RENDERING_VULKAN_VULKAN_HPP
+#define __FRAMERAT_CORE_RENDERING_VULKAN_VULKAN_HPP
+
+#include "VulkanRenderingContext.hpp"
+
+namespace framerat::core::rendering::vulkan {}
+
+#endif // __FRAMERAT_CORE_RENDERING_VULKAN_VULKAN_HPP
